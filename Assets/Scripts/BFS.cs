ea69e033a7cbb18cff0dd71c34849c3e7902de34
fredrikhanses <fredrik.hanses@gmail.com>
2019-09-27 15:35:52 +0200

    Moved BFS to its own script.

---
+++ Assets/Scripts/BFS.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+public class Graph<Location>
+{
+    public Dictionary<Location, Location[]> edges = new Dictionary<Location, Location[]>();
+    public Location[] Neighbors(Location id)
+    {
+        return edges[id];
+    }
+};
+
+public class BreadthFirstSearch : MonoBehaviour
+{
+    static void Print(ref LinkedList<string> path)
+    {
+        foreach (string data in path)
+        {
+            Console.Write($"{data}, ");
+        }
+    }
+
+    public static LinkedList<int> BreadthSearchNumbers(ref Graph<int> graph, ref int[] obstacleArrayIndex, int start, int goal, int mapSize)
+    {
+        Queue<int> frontier = new Queue<int>();
+        LinkedList<int> path = new LinkedList<int>();
+        int current = goal;
+        frontier.Enqueue(start);
+        int[] cameFrom = new int[mapSize];
+        for (int i = 0; i < cameFrom.Length; i++)
+        {
+            cameFrom[i] = mapSize;
+        }
+        while (frontier.Any())
+        {
+            current = frontier.Dequeue();
+            if (current == goal)
+            {
+                break;
+            }
+            foreach (int next in graph.Neighbors(current))
+            {
+                if (!cameFrom.Contains(next) && !obstacleArrayIndex.Contains(next))
+                {
+                    frontier.Enqueue(next);
+                    cameFrom[next] = current;
+                }
+            }
+        }
+        if (current == goal)
+        {
+            while (current != start)
+            {
+                path.AddLast(current);
+                current = cameFrom[current];
+            }
+            return path;
+        }
+        else
+        {
+            return path;
+        }
+    }
+}
+
+public class LinearSearch
+{
+    public static int LinearSearchSimple(ref Vector3[] dataArray, Vector3 data)
+    {
+        for (int index = 0; index < dataArray.Length; index++)
+        {
+            if (dataArray[index] == data)
+            {
+                return index;
+            }
+        }
+        return -1;
+    }
+}
--- Assets/Scripts/Search.cs
+++ Assets/Scripts/Search.cs
@@ -1,86 +1,9 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 using System;
-using System.Linq;
 
 namespace Search
 {
-    public class Graph<Location>
-    {
-        public Dictionary<Location, Location[]> edges = new Dictionary<Location, Location[]>();
-        public Location[] Neighbors(Location id)
-        {
-            return edges[id];
-        }
-    };
-
-    public class BreadthFirstSearch : MonoBehaviour
-    {
-        static void Print(ref LinkedList<string> path)
-        {
-            foreach (string data in path)
-            {
-                Console.Write($"{data}, ");
-            }
-        }
-
-        public static LinkedList<int> BreadthSearchNumbers(ref Graph<int> graph, ref int[] obstacleArrayIndex, int start, int goal, int mapSize)
-        {
-            Queue<int> frontier = new Queue<int>();
-            LinkedList<int> path = new LinkedList<int>();
-            int current = goal;
-            frontier.Enqueue(start);
-            int[] cameFrom = new int[mapSize];
-            for (int i = 0; i < cameFrom.Length; i++)
-            {
-                cameFrom[i] = mapSize;
-            }
-            while (frontier.Any())
-            {
-                current = frontier.Dequeue();
-                if (current == goal)
-                {
-                    break;
-                }
-                foreach (int next in graph.Neighbors(current))
-                {
-                    if (!cameFrom.Contains(next) && !obstacleArrayIndex.Contains(next))
-                    {
-                        frontier.Enqueue(next);
-                        cameFrom[next] = current;
-                    }
-                }
-            }
-            if (current == goal)
-            {
-                while (current != start)
-                {
-                    path.AddLast(current);
-                    current = cameFrom[current];
-                }
-                return path;
-            }
-            else
-            {
-                return path;
-            }
-        }
-    }
-
-    public class LinearSearch
-    {
-        public static int LinearSearchSimple(ref Vector3[] dataArray, Vector3 data)
-        {
-            for (int index = 0; index < dataArray.Length; index++)
-            {
-                if (dataArray[index] == data)
-                {
-                    return index;
-                }
-            }
-            return -1;
-        }
-    }
+    
 
     // A* needs only a WeightedGraph and a location type L, and does *not*
     // have to be a grid. However, in the example code I am using a grid.
